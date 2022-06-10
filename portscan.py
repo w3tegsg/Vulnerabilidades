6c0383d477dea445f6350ece751a53aabbbdaa39
João Victor <52130373+VictoRodrigues92@users.noreply.github.com>
2022-06-10 14:31:47 -0300

    Add files via upload
    Durante o curso introdutório de Ethical Hacker, foi criado duas ferramentas em Python pra ajudar no rastreamento de vulnerabilidades.

---
+++ portscan.py
@@ -0,0 +1,10 @@
+import socket
+
+ports = [21,22,80,443,445,3306,25]
+
+for port in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	client.settimeout(0.1)
+	code = client.connect_ex(("bancocn.com", port))
+	if code == 0:
+		print(port, "OPEN")
